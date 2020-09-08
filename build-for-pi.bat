e2a11c5c414fbe59512f180d5d3e0957c728ab15
Kjhf <kristian.f@hotmail.co.uk>
2020-09-09 00:40:19 +0100

    better pi build

--- build-for-pi.bat
+++ build-for-pi.bat
@@ -1,3 +1,5 @@
-cd "C:\Users\Kristian\source\repos\SlateBot"
-dotnet publish -r linux-arm
+cd ./SlateBot
+dotnet publish -r linux-arm -o "../release"
+cd ../
+tar.exe -cvzf _pi.tar "./release"
 pause
